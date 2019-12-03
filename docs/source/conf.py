ac5dc990ec2bd9eac5774d995c91710d4bab1c33
Ivan Sepulveda <ivan.sepulveda2014@gmail.com>
2019-12-02 21:16:19 -0800

    functionality updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-import mock
-MOCK_MODULES = ['pypylon']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+#import mock
+#MOCK_MODULES = ['pypylon']
+#for mod_name in MOCK_MODULES:
+    #sys.modules[mod_name] = mock.Mock()
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('..'))
