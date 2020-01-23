eb2ebfcc64ac92b89a656327b9c53c67af58a744
fwessel <fwessel@gmail.com>
2020-01-22 20:17:33 -0800

    moving averages

---
+++ src/moving_average.py
@@ -0,0 +1,55 @@
+from tkinter import Tk
+from tkinter import filedialog as fd
+from tkinter.filedialog import askopenfilename
+import matplotlib.pyplot as plt
+from scipy import ndimage, misc
+import os
+from os import path
+import sys
+import cv2
+import pandas
+from path_management import image_management as im
+import numpy as np
+import csv
+
+
+start_dir = os.getcwd()
+quit_string = "\nTo quit, type 'q' or 'quit', then press Enter: "
+print("Welcome to moving_average.py")
+user_input = input("To proceed and select an R_matrix to average, press Enter." + quit_string)
+
+if user_input.lower() in ["quit", "q"]:
+    sys.exit()
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+filename_sh = filename.split("/")[-1][:-4]
+
+run_directory = os.path.abspath(os.path.join(filename, os.pardir))
+
+print("You've selected: {}".format(filename))
+size_of_avg = int(input("How many pixels would you like to average by? - "))
+processed_dir = os.path.join(run_directory, "moving_averages_{}".format(size_of_avg))
+
+print("Run Directory: {}".format(run_directory))
+if not os.path.exists(processed_dir):
+    os.chdir(run_directory)
+    os.mkdir(processed_dir)
+    os.chdir(start_dir)
+
+
+csv_file = pandas.read_csv(filename,header=None)
+values = csv_file.values
+values = np.array(values, dtype='float32')
+
+result = ndimage.uniform_filter(values, size=size_of_avg, mode='constant')
+
+os.chdir(start_dir)
+
+csv_path = os.path.join(processed_dir, "{}_avg_{}.csv".format(filename_sh, size_of_avg))
+print("Averaged Array will be saved to: {}".format(csv_path))
+with open(csv_path, "w+", newline='') as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerows(result.tolist())
+
+os.chdir(start_dir)
--- src/scratch.py
+++ src/scratch.py
@@ -1,75 +1,13 @@
-#This program outputs 3D plots for Rmax, Rmin, and Rmax - Rmin.
-
-import plotly.graph_objects as go
-import pandas as pd
 import numpy as np
-import os
+from scipy import ndimage, misc
 
-#https://plot.ly/python/3d-surface-plots/
-#from pandas import DataFrame
-# Read data from a csv
+one = np.arange(0, 5)
+two = np.arange(5, 10)
+three = np.arange(10, 15)
+four = np.arange(15, 20)
+five = np.arange(20, 25)
 
-current_directory = os.getcwd()
-tests_directory = os.path.join(current_directory, "tests")
-run_directory = os.path.join(tests_directory, "2020_01_16__11_59")
-
-#Rmax = '../2020_01_16_11_59/r_matrix_30.csv'
-Rmax = os.path.join(run_directory, 'r_matrix_30.csv')
-Rmax_data = pd.read_csv(Rmax)
-R_max = Rmax_data.values
-
-#Rmin = '../2020_01_16_11_59/r_matrix_60.csv'
-Rmin = os.path.join(run_directory, 'r_matrix_60.csv')
-print("Does R Max path exist: {}".format(os.path.exists(Rmin)))
-
-Rmin_data = pd.read_csv(Rmin)
-R_min = Rmin_data.values
-
-sh_1, sh_2 = Rmax_data.shape
-x1, y1 = np.linspace(0, 1, sh_1), np.linspace(0, 1, sh_1)
-
-fig = go.Figure(data=[go.Surface(z=R_max, x=x1, y=y1)])
-fig.update_layout(title=(Rmax), autosize=False,
-                   width=1000, height=1000,
-                   margin=dict(l=100, r=50, b=50, t=90))
-
-fig0 = go.Figure(data=[go.Surface(z=R_min, x=x1, y=y1)])
-fig0.update_layout(title=(Rmin), autosize=False,
-                   width=1000, height=1000,
-                   margin=dict(l=100, r=50, b=50, t=90))
-
-Rmax_Rmin = np.subtract(Rmax_data, Rmin_data)
-RmaxRmin = np.multiply(Rmax_data, Rmin_data)
-numerator = np.subtract(1, RmaxRmin)
-k = np.divide(numerator, Rmax_Rmin)
-
-ksquared = np.square(k)
-ksquaredMinus1 = np.subtract(ksquared,1)
-V = np.subtract(k, ksquaredMinus1)
-
-VminusR = np.subtract(V, R_max)
-Denominator = np.multiply(V, R_max)
-alpha = np.divide(VminusR, Denominator)
-
-
-fig1 = go.Figure(data=[go.Surface(z=k, x=x1, y=y1)])
-fig1.update_layout(title=('k'), autosize=False,
-                   width=1000, height=1000,
-                   margin=dict(l=100, r=50, b=50, t=90))
-
-fig2 = go.Figure(data=[go.Surface(z=V, x=x1, y=y1)])
-fig2.update_layout(title = ('V'), autosize=False,
-                  width=1000, height=1000,
-                  margin=dict(l=100, r=50, b=50, t=90))
-
-fig3 = go.Figure(data=[go.Surface(z=alpha, x=x1, y=y1)])
-fig3.update_layout(title = ('alpha'), autosize=False,
-                  width=1000, height=1000,
-                  margin=dict(l=100, r=50, b=50, t=90))
-fig.show()
-fig0.show()
-fig1.show()
-fig2.show()
-fig3.show()
-
-# export_csv = df.to_csv('r_matrix_difference.csv', index=None, header=True)
+stacked = np.array(np.vstack((one, two, three, four, five)), dtype='float32')
+print(stacked)
+print("\n")
+print(ndimage.uniform_filter(stacked, size=3, mode='constant'))
