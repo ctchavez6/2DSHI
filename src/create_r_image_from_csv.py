14a180ab24e2d8369313fe5f021fe1e8201cb334
fwessel <fwessel@gmail.com>
2020-02-29 20:56:55 -0800

    create_r_image_from_csv.py: changed prompt to "an appropriate file" from Rmin Rmax

--- src/create_r_image_from_csv.py
+++ src/create_r_image_from_csv.py
@@ -22,7 +22,7 @@ ignored_states = np.seterr(**old_err_state)
 start_dir = os.getcwd()
 quit_string = "\nTo quit, type 'q' or 'quit', then press Enter: "
 print("Welcome to create r matrix from csv.py")
-user_input = input("To proceed and select R Min & R Max, press Enter." + quit_string)
+user_input = input("To proceed and select an appropriate file, press Enter." + quit_string)
 
 if user_input.lower() in ["quit", "q"]:
     sys.exit()
