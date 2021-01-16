02877877606db647d3d49a7ea8136efded2ad826
fwessel <fwessel@gmail.com>
2021-01-16 15:43:39 -0800

    modularized step 6c

--- src/stream_tools/s6.py
+++ src/stream_tools/s6.py
@@ -38,6 +38,7 @@ def step_six_a(stream, continue_stream):
 
     cv2.destroyAllWindows()
 
+
 def step_six_b(stream, continue_stream, app):
     find_rois_ = input("Step 6B - Re-Coregister - {}".format(y_n_msg))
 
@@ -85,3 +86,41 @@ def step_six_b(stream, continue_stream, app):
         continue_stream = False
 
     cv2.destroyAllWindows()
+
+def step_six_c(stream, continue_stream):
+    display_new = input("Step 6C - Display Re-Coregistered Images - {}".format(y_n_msg))
+
+    if display_new.lower() == "y":
+        continue_stream = True
+
+    cv2.destroyAllWindows()
+
+    while continue_stream:
+        stream.frame_count += 1
+        stream.current_frame_a, stream.current_frame_b = stream.grab_frames(warp_matrix=stream.warp_matrix)
+
+        x_a, y_a = stream.static_center_a
+        x_b, y_b = stream.static_center_b
+
+        n_sigma = 3.0
+
+        stream.roi_a = stream.current_frame_a[
+                     int(y_a - n_sigma * stream.static_sigmas_y): int(y_a + n_sigma * stream.static_sigmas_y) + 1,
+                     int(x_a - n_sigma * stream.static_sigmas_x): int(x_a + n_sigma * stream.static_sigmas_x) + 1]
+
+        stream.roi_b = stream.current_frame_b[
+                     int(y_b - n_sigma * stream.static_sigmas_y): int(y_b + n_sigma * stream.static_sigmas_y) + 1,
+                     int(x_b - n_sigma * stream.static_sigmas_x): int(x_b + n_sigma * stream.static_sigmas_x) + 1]
+
+        cv2.imshow("ROI A", bdc.to_16_bit(stream.roi_a))
+        cv2.imshow("ROI B Prime", bdc.to_16_bit(stream.roi_b))
+
+        if stream.warp_matrix_2 is None:
+            roi_a = stream.roi_a
+            b_double_prime = stream.roi_b
+        else:
+            roi_a, b_double_prime = stream.grab_frames2(stream.roi_a.copy(), stream.roi_b.copy(), stream.warp_matrix_2.copy())
+
+        cv2.imshow("ROI B DOUBLE PRIME", bdc.to_16_bit(b_double_prime))
+
+        continue_stream = stream.keep_streaming()
