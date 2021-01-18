98c75c743fbac626122766f2e1902cc98edb2940
fwessel <fwessel@gmail.com>
2021-01-18 12:31:05 -0800

    modularized step 10

--- src/stream_tools/stream_tools.py
+++ src/stream_tools/stream_tools.py
@@ -1383,16 +1383,15 @@ class Stream:
         self.all_cams.StopGrabbing()
 
         step = 10
-        #s10.step_ten(run_folder)
-
+        s10.step_ten(run_folder)
+        """
         notes = input("Step 10 - Write some notes to a file? - Proceed? (y/n): ")
-
         if notes.lower() == 'y':
             notes = input("Write notes below:\n\n")
             notes_file = open(os.path.join(run_folder, 'notes.txt'), 'w+')
             notes_file.write(notes)
             notes_file.close()
-        """
+        
         """
         step = 11
         s11.step_eleven(self, run_folder)
