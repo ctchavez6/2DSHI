14958987de92b20fd6e47cae2919e5f0f57ef22e
fwessel <fwessel@gmail.com>
2021-03-19 17:39:48 -0700

    killing dead code and print statements

--- src/stream_tools/App.py
+++ src/stream_tools/App.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import threading
 import traceback
-import tkinter.ttk as ttk
 import os
 import pickle
 
@@ -20,36 +19,23 @@ class App(threading.Thread):
         prev_h_offset_path = os.path.join(stream_object.prv_run_dir, "h_offset.p")
         prev_h_offset_exists = os.path.exists(prev_h_offset_path)
 
-
-        print("prev_n_sigma_exists: ", prev_n_sigma_exists)
         if prev_n_sigma_exists and stream_object is not None:
             with open(prev_n_sigma_path, 'rb') as fp:
                 self.foo = pickle.load(fp)
-                print("prev_v_sigma: ", self.foo)
-
         else:
             self.foo = 1
 
-        print("prev_v_offset_exists: ", prev_h_offset_exists)
         if prev_v_offset_exists:
             with open(prev_v_offset_path, 'rb') as fp:
                 self.vertical_offset = pickle.load(fp)
-                print("prev_v_offset: ", self.vertical_offset)
-
         else:
             self.vertical_offset = 0
 
-        print("prev_h_offset_exists: ", prev_h_offset_exists)
         if prev_h_offset_exists:
             with open(prev_h_offset_path, 'rb') as fp:
                 self.horizontal_offset = pickle.load(fp)
-                print("prev_h_offset: ", self.horizontal_offset)
         else:
             self.horizontal_offset = 0
-
-        # NOT LOADING PREV N_SIGMA or V/H OFFSETS
-        # PRINT LINE DEBUG To FIND OUT WHYYYYY?
-
 
         self.start()
 
@@ -79,14 +65,9 @@ class App(threading.Thread):
 
     def toggle(self):
         if self.stop_streaming_override is True:
-                #self.toggle_btn.config('relief')[-1] == 'sunken':
             self.stop_streaming_override = False
-            #self.toggle_btn.config(relief="raised")
-            #print("self.is_toggle_pressed_down: ", self.is_toggle_pressed_down)
         else:
-            #self.toggle_btn.config(relief="sunken")
             self.stop_streaming_override = True
-            #print("self.is_toggle_pressed_down: ", self.is_toggle_pressed_down)
 
     def move_roi_left(self):
         self.horizontal_offset += self.ofset_delta
@@ -107,7 +88,6 @@ class App(threading.Thread):
         label = tk.Label(self.root, text="Sigma")
         label.pack()
 
-        print("App.run() -> slider max is : ", self.slider_maximum)
 
         if self.slider_maximum > 1.00:
             self.sigma_slider = tk.Scale(from_=1.00, to=self.slider_maximum, tickinterval=0.0001, resolution = 0.25, digits = 3,orient=tk.HORIZONTAL, command=self.scale_onChange)
@@ -115,9 +95,6 @@ class App(threading.Thread):
 
         else:
             self.sigma_slider = tk.Scale(from_=1.00, to=1.00, tickinterval=0.0001, resolution = 0.25, digits = 3,orient=tk.HORIZONTAL, command=self.scale_onChange)
-            #self.sigma_slider = tk.Scale(from_=1.00, to=2.50, tickinterval=0.0001, resolution = 0.25, digits = 3,orient=tk.HORIZONTAL, command=self.scale_onChange)
-            #self.sigma_slider.set(self.foo)
-            #self.sigma_slider.pack()
 
         self.sigma_slider.set(self.foo)
         self.sigma_slider.pack()
@@ -139,12 +116,6 @@ class App(threading.Thread):
 
         self.down_offset_button = tk.Button(repeatdelay=250, repeatinterval=100, text="v", width=12, relief="raised", command=self.move_roi_down)
         self.down_offset_button.pack()
-
-
-        #tk.Button(repeatdelay=250, repeatinterval=100, text="<", width=12, relief="raised", command=self.move_roi_right).pack()
-        #tk.Button(repeatdelay=250, repeatinterval=100, text=">", width=12, relief="raised", command=self.move_roi_left).pack()
-        #tk.Button(repeatdelay=250, repeatinterval=100, text="^", width=12, relief="raised", command=self.move_roi_up).pack()
-        #tk.Button(repeatdelay=250, repeatinterval=100, text="v", width=12, relief="raised", command=self.move_roi_down).pack()
 
         self.root.mainloop()
 
